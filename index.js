d90016828002347a42d8ac90ab8b372737e7a4e5
MicroCBer <66859419+MicroCBer@users.noreply.github.com>
2022-08-27 17:45:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 !(async () => {
-    let platform={
-        "localhost:5500":"localhost",
-        "127.0.0.1:5500":"localhost",
-        "microblock.ink":"Github Pages",
-    }[document.location.host]||"Unknown";
-    if(document.location.host.includes("zeyu"))platform="WGzeyu"
+    let platform = {
+        "localhost:5500": "localhost",
+        "127.0.0.1:5500": "localhost",
+        "microblock.ink": "Github Pages",
+    }[document.location.host] || "Unknown";
+    if (document.location.host.includes("zeyu")) platform = "WGzeyu"
     $(".meta").text(`版本：0.13，前端：MicroBlock，后端：WGzeyu，服务器：${platform}，数据源：QosmeticsDiscord.`)
-    if(platform=="WGzeyu")$(".contact").text("If you don't want your models to appear on this website, please contact WGzeyu at Discord WGzeyu#7287 or 785777793@qq.com")
+    if (platform == "WGzeyu") $(".contact").text("If you don't want your models to appear on this website, please contact WGzeyu at Discord WGzeyu#7287 or 785777793@qq.com")
 
     function xss(str) {
         return str.replaceAll("<", "&lt;").replaceAll(">", "&gt;")
@@ -24,14 +24,14 @@
     }
 
     let sabersData = (await (await fetch("data/qsabers-web.json")).json())["model"],
-        sabers=[],filters={},filteredHandle=()=>{sabers.sort((a,b)=>{ return b["uploadtime"]-a["uploadtime"]})};
-    let handle=null;
+        sabers = [], filters = {}, filteredHandle = () => { sabers.sort((a, b) => { return b["uploadtime"] - a["uploadtime"] }) };
+    let handle = null;
 
-    function filterModels(){
-        sabers=[]
-        model:for(let model of sabersData){
-            for(let filter of Object.values(filters))
-                if(!(filter(model)))continue model;
+    function filterModels() {
+        sabers = []
+        model: for (let model of sabersData) {
+            for (let filter of Object.values(filters))
+                if (!(filter(model))) continue model;
             sabers.push(model);
         }
         filteredHandle()
@@ -72,10 +72,10 @@
                     dom.find(".img").remove();
                     dom.find(".img-no-image").addClass("shown");
                 } else {
-                    if(platform=="Github Pages")dom.find(".img").attr("data-gisrc", "https://raw.githubusercontent.com/MicroCBer/ModelViewer/main/"+model["imgurl"])
-                    else 
-                    dom.find(".img").attr("src", model["imgurl"])
-                     
+                    if (platform == "Github Pages") dom.find(".img").attr("data-gisrc", "https://raw.githubusercontent.com/MicroCBer/ModelViewer/main/" + model["imgurl"])
+                    else
+                        dom.find(".img").attr("src", model["imgurl"])
+
                 }
 
                 dom.find(".name").text(decodeUnicode(model["name"]))
@@ -92,13 +92,13 @@
                     document.location.assign(model["modelurl"])
                 })
             }
-            if(platform=="Github Pages")
-            $("img[data-gisrc]").gazeimg({bg:['#000']})
+            if (platform == "Github Pages")
+                $("img[data-gisrc]").gazeimg({ bg: ['#000'] })
         }
 
         let loadLock = false;
-        if(handle)removeEventListener("scroll",handle);
-        handle=addEventListener("scroll", () => {
+        if (handle) removeEventListener("scroll", handle);
+        handle = addEventListener("scroll", () => {
             if (window.scrollY > document.body.scrollHeight - 1500 && !loadLock) {
                 loadLock = true
                 maxIndex = Math.min(sabers.length, maxIndex + 10);
@@ -110,56 +110,62 @@
     }
     initModels()
 
-    $("#gqui-imageonly").on("change",function(){
-        if(this.checked)filters["imageonly"]=(model)=>model["imgurl"]?true:false;
+    $("#gqui-imageonly").on("change", function () {
+        if (this.checked) filters["imageonly"] = (model) => model["imgurl"] ? true : false;
         else delete filters["imageonly"]
         initModels()
     })
 
-    $("#gqui-timesort").on("change",function(){
-        filteredHandle=()=>{sabers.sort((a,b)=>{
-            return a["uploadtime"]-b["uploadtime"]
-        })}
+    $("#gqui-timesort").on("change", function () {
+        filteredHandle = () => {
+            sabers.sort((a, b) => {
+                return a["uploadtime"] - b["uploadtime"]
+            })
+        }
         initModels()
     })
-    $("#gqui-timesort-reversed").on("change",function(){
-        filteredHandle=()=>{sabers.sort((a,b)=>{
-            return b["uploadtime"]-a["uploadtime"]
-        })}
+    $("#gqui-timesort-reversed").on("change", function () {
+        filteredHandle = () => {
+            sabers.sort((a, b) => {
+                return b["uploadtime"] - a["uploadtime"]
+            })
+        }
         initModels()
     })
-    $("#gqui-unsort").on("click",function(){
-        filteredHandle=()=>{sabers.sort((a,b)=>{
-            return Math.random()-0.5
-        })}
+    $("#gqui-unsort").on("click", function () {
+        filteredHandle = () => {
+            sabers.sort((a, b) => {
+                return Math.random() - 0.5
+            })
+        }
         initModels()
     })
-    $("#gqui-search").on("change",function(){
+    $("#gqui-search").on("change", function () {
 
-        if(this.checked){
-            search.disabled=true;
-            filters["search"]=(model)=>{
+        if (this.checked) {
+            search.disabled = true;
+            filters["search"] = (model) => {
                 return model["description"].includes(search.value)
             };
         }
-        else{
+        else {
             delete filters["search"]
-            search.disabled=false
-        } 
+            search.disabled = false
+        }
         initModels()
     })
 
-    $(".btnSubtitle").click(async function(){
-        if($(this).hasClass("activated"))return;
+    $(".btnSubtitle").click(async function () {
+        if ($(this).hasClass("activated")) return;
         $(".btnSubtitle").removeClass("activated");
         $(this).addClass("activated");
-        sabersData = (await (await fetch(this.dataset.file)).json())["model"]
+        sabersData = (await (await fetch(this.dataset.file + "?" + new Date().getTime())).json())["model"]
         $("#sabers").fadeOut(200)
-        setTimeout(()=>{
+        setTimeout(() => {
             initModels()
             $("#sabers").fadeIn(200)
-        },200)
-        
-        
+        }, 200)
+
+
     })
 })()
